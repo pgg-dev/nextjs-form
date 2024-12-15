10c39f5457d8516169866cc0f27d563903013514
pgg-dev <park.geumgyu@gmail.com>
2024-12-16 03:57:24 +0900

    fix prisma

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import TweetList from "@/components/tweet-list";
 import { getInitialTweets } from "../service/tweetService";
 import AddTweet from "../components/add-tweet";
+import { neon } from "@neondatabase/serverless";
 
 export default async function MainPage() {
   const tweets = await getInitialTweets();
+
   return (
     <div className="p-5 flex flex-col gap-5">
       <AddTweet />
